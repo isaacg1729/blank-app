8a36b175b605aa06c45831984267cd17b44a72d2
Isaac Paul <shane.isaacpaul@gmail.com>
2025-11-08 13:31:34 +0530

    Checking again

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app is working")
+st.title("🎈 My new app is working 12")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
