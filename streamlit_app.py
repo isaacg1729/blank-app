879eb24fc1eddd65c3f30304fdc083e486fb1779
Isaac Paul <shane.isaacpaul@gmail.com>
2025-11-08 13:34:06 +0530

    Taking Input

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ st.title("🎈 My new app is working 12")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+X = 10
+SS = st.number_input("Enter a number")
+st.write("Number :",SS)
